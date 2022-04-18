64f710d348f6c988ee7fdad1aef8f68c098969ef
Walter Santacruz <wsantacruz@technisys.com>
2022-04-18 12:55:27 -0500

    spinner,form and dance

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,12 +1,65 @@
 import React from "react";
 import "./index.scss";
+import emailjs from "emailjs-com";
+import Swal from "sweetalert2";
+import Spinner from "../Spinner";
+type message = {
+    correo: string;
+    mensaje: string;
+};
+
 const Form = () => {
+    const form = React.useRef<HTMLFormElement>(null);
+    const [show, setShow] = React.useState<boolean>(false);
+    const [formData, setFormData] = React.useState<message>({
+        correo: "",
+        mensaje: "",
+    });
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setShow(true);
+        emailjs
+            .send(
+                "service_curriculum",
+                "template_8l9v4m9",
+                {
+                    correo: formData.correo,
+                    mensaje: formData.mensaje,
+                },
+                "user_pTLxySAThdUhCksIGYy0E"
+            )
+            .then((res) => {
+                console.log(res);
+                Swal.fire({
+                    title: "Correo enviado Exitosamente",
+                    width: 600,
+                    padding: "3em",
+                    color: "#ededed",
+                    icon: "success",
+                    background: "#2c394b",
+                });
+            })
+            .catch((err) => {
+                Swal.fire({
+                    title: "Error al enviar el correo",
+                    width: 600,
+                    padding: "3em",
+                    color: "#ededed",
+                    icon: "error",
+                    background: "#2c394b",
+                });
+            })
+            .finally(() => {
+                setFormData({
+                    correo: "",
+                    mensaje: "",
+                });
+                setShow(false);
+            });
     };
 
     return (
-        <form className="formulario" onSubmit={handleSubmit}>
+        <form ref={form} className="formulario" onSubmit={handleSubmit}>
             <h1>Contácteme</h1>
             <div className="grupo-formulario">
                 <label htmlFor="correo">Correo Electrónico</label>
@@ -14,8 +67,16 @@ const Form = () => {
                     type="email"
                     className=""
                     id="correo"
+                    name="correo"
                     aria-describedby="emailHelp"
                     placeholder="Enter email"
+                    onChange={(e) =>
+                        setFormData({
+                            correo: e.target.value,
+                            mensaje: formData.mensaje,
+                        })
+                    }
+                    value={formData.correo}
                 />
             </div>
             <div className="grupo-formulario">
@@ -26,14 +87,25 @@ const Form = () => {
                     maxLength={100}
                     className=""
                     id="mensaje"
+                    name="mensaje"
                     aria-describedby="emailHelp"
                     placeholder="Enter email"
+                    onChange={(e) =>
+                        setFormData({
+                            correo: formData.correo,
+                            mensaje: e.target.value,
+                        })
+                    }
+                    value={formData.mensaje}
                 />
             </div>
             <div className="d-flex justify-content-center align-items-center">
                 <button type="submit" className="mt-2 boton-enviar">
                     Enviar
                 </button>
+            </div>
+            <div className="d-flex justify-content-center align-items-center mt-2">
+                {show && <Spinner></Spinner>}
             </div>
         </form>
     );
---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./index.scss";
+const Spinner = () => {
+    return <span className="spinner-span"></span>;
+};
+
+export default Spinner;
